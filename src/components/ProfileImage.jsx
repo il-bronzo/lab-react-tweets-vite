cb65e52aa171b8cf5cd1499c09d043d58a3a0be5
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-11-17 00:22:51 +0100

    Completed until Iteration 3

---
+++ src/components/ProfileImage.jsx
@@ -0,0 +1,13 @@
+function ProfileImage (props) {
+    return (
+        <>
+        <img
+        src={props.src}
+        className="profile"
+        alt="profile"
+      />
+        </>
+    )
+}
+
+export default ProfileImage;
